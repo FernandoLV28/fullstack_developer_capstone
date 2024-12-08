afecb28f787c43fc5bb25e5da56932b18820ed63
FernandoLV28 <flopez28dev@gmail.com>
2024-12-08 16:22:51 -0600

    `Update settings.py and urls.py with new allowed hosts and CSRF trusted origins`

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
-
-
-# ALLOWED_HOSTS = ['localhost', 'https://fernandolope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-# CSRF_TRUSTED_ORIGINS = ['https://fernandolope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['localhost', 'https://fernandolope-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://fernandolope-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
