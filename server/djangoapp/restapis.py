2c069f4be4aa133985151a34e9d67bf39c8949f7
FernandoLV28 <flopez28dev@gmail.com>
2024-12-13 19:51:43 -0600

    `Added get_request function and uncommented import statements in restapis.py`

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,8 @@
 # Uncomment the imports below before you add the function code
 # import requests
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
+import requests # type: ignore
 
 load_dotenv()
 
@@ -13,6 +14,22 @@ sentiment_analyzer_url = os.getenv(
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+
+    request_url = backend_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
